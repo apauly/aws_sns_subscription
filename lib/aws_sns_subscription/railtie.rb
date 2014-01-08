cd3da96d2d308cc25416b068ccc6ecd0ba55822e
Alexander Pauly <alex.pauly@gmx.de>
2014-01-08 20:12:09 +0100

    removed debug message

--- lib/aws_sns_subscription/railtie.rb
+++ lib/aws_sns_subscription/railtie.rb
@@ -2,7 +2,6 @@ module AWSSNSSubscription
   class Railtie < Rails::Railtie
     initializer "aws_sns_subscription.action_controller" do
       ActiveSupport.on_load(:action_controller) do
-        puts "Extending #{self} with AWSSNSSubscription::Confirmer"
         include AWSSNSSubscription::Confirmer
       end
     end
